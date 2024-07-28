918d9537c5edc4f209adea2ba4cd14b5b9961eec
Ezra <62970784+Etoad8473@users.noreply.github.com>
2024-07-27 23:39:08 -0500

    addTransaction component, begin global state setup

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export const AddTransaction = () => {
+
+  const [text, setText] = useState('');
+  const [amount, setAmount] = useState(0);
+
   return (
     <>
       <h3>Add new transaction</h3>
       <form action="">
         <div className="form-control">
             <label htmlFor="text">Text</label>
-            <input type="text" placeholder='Enter text...' />
+            <input 
+              type="text" 
+              placeholder='Enter text...' 
+              value={text} 
+              onChange={(e)=>{setText(e.target.value)}}
+            />
         </div>
         <div className="form-control">
-            <label htmlFor="amount">Amount<br/>(negative - expense, postiive - income)</label>
+            <label htmlFor="amount">Amount<br/>(negative - expense, positive - income)</label>
+            <input 
+              type="number" 
+              placeholder='Enter amount...' 
+              value={amount} 
+              onChange={(e)=>{setAmount(e.target.value)}}
+            />
         </div>
       </form>
     </>
