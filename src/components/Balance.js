2c87e17cba6f0ccc64d9a567321c0a720e40b38a
Ezra <62970784+Etoad8473@users.noreply.github.com>
2024-07-29 01:34:20 -0500

    balance correctly summing

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 export const Balance = () => {
+
+  const {transactions} = useContext(GlobalContext);
+  const values = transactions.map(transactions => (transactions.amount));
+  const sum = values.reduce((a,b)=>a+b).toFixed(2);
+
   return (
     <>
         <h4>Your balance</h4>
-        <h1>$0.00</h1>
+        <h1>${sum}</h1>
     </>
   )
 }
