de38a2555f9e55979ad4599f7c691a443d258096
Ezra <62970784+Etoad8473@users.noreply.github.com>
2024-08-05 18:44:57 -0500

    remove '$' from transactions

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -9,7 +9,7 @@ export const Transaction = ({transaction}) => {
   return (
     <div>
         <li className="{transaction.amount >= 0 ? 'plus' : 'minus'}">
-            {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span>
+            {transaction.text} <span>{sign}{Math.abs(transaction.amount)}</span>
             <button className="delete-btn" onClick={()=>deleteTransaction(transaction.id)}>x</button>
         </li>
     </div>
